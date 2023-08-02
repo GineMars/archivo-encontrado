a84138c7187eabaa5de7c82c7a48a2eb1fde63e4
GineMars <141115374+GineMars@users.noreply.github.com>
2023-08-02 04:19:33 -0500

    initial commit

---
+++ funs.js
@@ -0,0 +1,7 @@
+ function fun(){
+
+              
+              if (document.getElementById('arch').value=="DrawBobRossHOME"){
+                  window.location.href = "https://docs.google.com/document/d/11t6LtpR1Z8MQT4ngjg8uCVzezeQkZ_tkw9lFbklmeyc/edit";
+              }
+          }
